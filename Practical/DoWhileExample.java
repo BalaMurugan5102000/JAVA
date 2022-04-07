f4bd45e454d79f939a51dddd36d88bb05d043279
BalaMurugan5102000 <102968254+BalaMurugan5102000@users.noreply.github.com>
2022-04-07 18:24:07 +0530

    Add files via upload

---
+++ Practical/DoWhileExample.java
@@ -0,0 +1,16 @@
+public class DoWhileExample{
+
+public void show(){
+int b=0;
+do{
+System.out.println("Value is:"+b);
+b=b-5;
+}
+while(b>=1);
+
+}
+public static void main(String args[]){
+DoWhileExample w=new DoWhileExample();
+w.show();
+}
+}
