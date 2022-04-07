c03d3d3a9d960ced038a15a7e875b053b4d4fbb5
BalaMurugan5102000 <102968254+BalaMurugan5102000@users.noreply.github.com>
2022-04-07 18:21:14 +0530

    Add files via upload

---
+++ Practical/ForLoopExample.java
@@ -0,0 +1,14 @@
+public class ForLoopExample{
+
+public void show(){
+for(int i=2; i<=10; i=i+2){
+System.out.println("Value is:"+i);
+}
+}
+
+
+public static void main(String args[]){
+ForLoopExample f=new ForLoopExample();
+f.show();
+}
+}
