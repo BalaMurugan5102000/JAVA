e2010838b82132ac97dcaad1cd10cb18bdac2b19
BalaMurugan5102000 <102968254+BalaMurugan5102000@users.noreply.github.com>
2022-04-08 16:20:32 +0530

    Add files via upload

---
+++ Practical/ArithmaticOperatorExample.java
@@ -0,0 +1,13 @@
+class ArithmaticOperatorExample{
+
+public static void main(String args[]){
+int a=55;
+int b=75;
+
+System.out.println("a+b="+(a+b));//addition
+System.out.println("a-b="+(a-b));//substraction
+System.out.println("a*b="+(a*b));//multiplication
+System.out.println("a/b="+(a/b));//division
+System.out.println("a%b="+(a%b));//modulus
+}
+}
