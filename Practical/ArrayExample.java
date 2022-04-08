1887ebf104b7c863723f841ebe16c57abef0692b
BalaMurugan5102000 <102968254+BalaMurugan5102000@users.noreply.github.com>
2022-04-08 16:21:00 +0530

    Add files via upload

---
+++ Practical/ArrayExample.java
@@ -0,0 +1,17 @@
+class ArrayExample{
+
+public static void main(String args[]){
+String[]names={"Rocky","Bala","Sam","Arun"};
+//System.out.println(names[3]);
+for (int i=0; i<names.length; i++){
+   System.out.println(names[i]);
+}
+
+System.out.println();
+
+int[]phone={123,345,567,789,787,765};
+    for (int j=0; j<phone.length; j++){
+	System.out.println(phone[j]);
+	}
+}
+}
