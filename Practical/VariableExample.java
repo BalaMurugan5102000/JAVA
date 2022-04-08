9b62dde38bb434d272b88020b6f4499eb072f6c5
BalaMurugan5102000 <102968254+BalaMurugan5102000@users.noreply.github.com>
2022-04-08 16:24:56 +0530

    Add files via upload

---
+++ Practical/VariableExample.java
@@ -0,0 +1,11 @@
+public class VariableExample{
+
+public static void main(String args[]){
+int ticketprize = 900;
+boolean paid = true;
+char c = 't';
+System.out.println(ticketprize);
+System.out.println(paid);
+System.out.println(c);
+}
+}
