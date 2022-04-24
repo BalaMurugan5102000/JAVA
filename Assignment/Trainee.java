fe9e51095f984975300bfe1e11caca205716c15e
BalaMurugan5102000 <102968254+BalaMurugan5102000@users.noreply.github.com>
2022-04-24 17:44:03 +0530

    Add files via upload

---
+++ Assignment/Trainee.java
@@ -0,0 +1,11 @@
+package Company;
+
+ class Trainee extends Employee {
+
+  Trainee(long Id, String Name, String Address, long Phone, double basicSalary){
+	  super(Id, Name, Address, Phone, basicSalary); 
+  }		 
+		 
+	 }
+
+
