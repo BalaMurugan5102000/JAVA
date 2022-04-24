9c69928d03340cfc1d3e8898542c8539cf740e58
BalaMurugan5102000 <102968254+BalaMurugan5102000@users.noreply.github.com>
2022-04-24 17:42:41 +0530

    Add files via upload

---
+++ Assignment/Manager.java
@@ -0,0 +1,11 @@
+package Company;
+
+class Manager extends Employee{
+	 
+	 
+	 Manager(long Id, String Name, String Address, long Phone, double basicSalary){
+		 
+		 super(Id, Name, Address, Phone, basicSalary);
+	 } 
+}
+ 
