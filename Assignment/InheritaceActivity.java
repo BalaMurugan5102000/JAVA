cd17dd69a06be5570fc9d0f2723b2fb30a17c9c8
BalaMurugan5102000 <102968254+BalaMurugan5102000@users.noreply.github.com>
2022-04-24 17:43:29 +0530

    Add files via upload

---
+++ Assignment/InheritaceActivity.java
@@ -0,0 +1,10 @@
+package Company;
+
+public class InheritaceActivity {
+
+	public static void main(String[] args) {
+		Manager m = new Manager(126534, "Peter", "Chennai,India", 237844, 65000);
+		m.CalculateSalary();
+	}
+
+}
