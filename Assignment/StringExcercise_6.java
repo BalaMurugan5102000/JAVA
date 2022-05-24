9e2fd0d55f180fba650e5f2967664cce94c562e3
BalaMurugan5102000 <102968254+BalaMurugan5102000@users.noreply.github.com>
2022-05-24 12:49:18 +0530

    Add files via upload

---
+++ Assignment/StringExcercise_6.java
@@ -0,0 +1,19 @@
+package java_activities;
+
+public class StringExcercise_6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str = "LEARN TO MAKE YORSELF HAPPY";
+		
+		// convert the above string into lowercase
+		
+		String lowerstr = str.toLowerCase();
+		
+		System.out.println("Original String :" +str);
+		System.out.println("String in lowercase :" + lowerstr);
+
+	}
+
+}
