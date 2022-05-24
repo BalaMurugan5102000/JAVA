17e8ad8dd4380ac8def7bfc5d9685e762678acfa
BalaMurugan5102000 <102968254+BalaMurugan5102000@users.noreply.github.com>
2022-05-24 10:09:23 +0530

    Add files via upload

---
+++ Assignment/UserNameValidation.java
@@ -0,0 +1,21 @@
+package java_activities;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class UserNameValidation {
+public static void main(String[] args) {
+Scanner s = new Scanner(System.in);
+System.out.println("Enter UserName :");
+String k = s.next();
+boolean b1 = Pattern.matches("^[A-Za-z_][A-Za-z0-9_]{8,30}$",k);
+if (b1==true) {
+System.out.println("Valid");
+} else {
+System.out.println("Invalid");
+}
+}
+}
+
