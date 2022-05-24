83975a0e8a24289c8e883d64815f4c9f6a9b321d
BalaMurugan5102000 <102968254+BalaMurugan5102000@users.noreply.github.com>
2022-05-24 12:49:43 +0530

    Add files via upload

---
+++ Assignment/StringExcercis_7.java
@@ -0,0 +1,19 @@
+package java_activities;
+
+public class StringExcercis_7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str = " Stay Humble ";
+		
+		//trim the white spaces from front and back of the string.
+		
+		String new_str = str.trim();
+		
+		System.out.println("Original String :" + str);
+		System.out.println("New Sttring :" + new_str);
+
+	}
+
+}
