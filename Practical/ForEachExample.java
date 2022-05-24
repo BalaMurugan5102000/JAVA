6b5356391294d61f933c8a5a09918a163aa29d9b
BalaMurugan5102000 <102968254+BalaMurugan5102000@users.noreply.github.com>
2022-05-24 10:06:25 +0530

    Add files via upload

---
+++ Practical/ForEachExample.java
@@ -0,0 +1,18 @@
+package java8eg;
+
+import java.util.ArrayList;
+import java.util.List;
+public class ForEachExample {
+
+public static void main(String[] args) {
+List<String> flower = new ArrayList<String>();
+flower.add("Rose");
+flower.add("Jasmine");
+flower.add("Sunflower");
+flower.add("MariGold");
+flower.add("Lilly");
+flower.forEach(display->System.out.println(display));//iterating using lambda expression
+flower.forEach(System.out::println);  //iterating with method reference
+}
+}
+
