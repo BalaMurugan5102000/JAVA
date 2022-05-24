db17e2b48b58b3bebe78775a29c909bc0c07b25f
BalaMurugan5102000 <102968254+BalaMurugan5102000@users.noreply.github.com>
2022-05-24 12:48:19 +0530

    Add files via upload

---
+++ Assignment/StringExcercise_4.java
@@ -0,0 +1,14 @@
+package java_activities;
+
+public class StringExcercise_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name = "Education";
+		int length = name.length();
+		System.out.println("The length of the String \"" + name +"\" is : "+length);
+
+	}
+
+}
